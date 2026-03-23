e3bf44793fa0af7bfd7f206ef280f9cf0162ca47
Keserű Márton <gergelymarton00@gmail.com>
2026-03-23 16:02:05 +0100

    Products Add Controller

---
+++ DepotMap.Backend/DepotMap.Endpoint/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using DepotMap.Entities.Models.DTOs.Products;
+using DepotMap.Logics.Logics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DepotMap.Endpoint.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ProductsLogic _productsLogic;
+        public ProductsController(ProductsLogic productsLogic)
+        {
+            _productsLogic = productsLogic;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct([FromBody] CreateProductDto dto)
+        {
+
+            await _productsLogic.CreateProductAsync(dto);
+            return Ok("Product created successfully.");
+
+        }
+    }
+}
--- DepotMap.Backend/DepotMap.Logics/Logics/ProductsLogic.cs
+++ DepotMap.Backend/DepotMap.Logics/Logics/ProductsLogic.cs
@@ -15,20 +15,20 @@ namespace DepotMap.Logics.Logics
     {
         private readonly AppDbContext _ctx;
         private readonly IMapper _mapper;
-        public ProductsLogic(AppDbContext ctx , IMapper mapper) 
+        public ProductsLogic(AppDbContext ctx, IMapper mapper)
         {
             this._ctx = ctx;
             _mapper = mapper;
         }
         public async Task CreateProductAsync(CreateProductDto dto)
-        { 
+        {
             var product = _mapper.Map<Product>(dto);
             _ctx.Products.Add(product);
             await _ctx.SaveChangesAsync();
         }
         public async Task UpdateProductAsync(string id, CreateProductDto dto, string userId)
         {
-           
+
             var product = await _ctx.Products.FindAsync(id);
             if (product == null) throw new Exception("Product not found");
             var history = _mapper.Map<ProductHistory>(product);
@@ -37,15 +37,31 @@ namespace DepotMap.Logics.Logics
             history.Timestamp = DateTime.Now;
             history.CreatedByUserId = userId;
 
-            _ctx.ProductHistories.Add(history); 
+            _ctx.ProductHistories.Add(history);
 
-           
+
             _mapper.Map(dto, product);
-            
+
             await _ctx.SaveChangesAsync();
         }
+        public async Task DeleteProductAsync(string id, string userId)
+        {
+           
+            var product = await _ctx.Products.FindAsync(id);
+            if (product == null) throw new Exception("Product not found");
+           
+            var historyEntry = _mapper.Map<ProductHistory>(product);
 
+            historyEntry.ActionType = "delete"; 
+            historyEntry.Timestamp = DateTime.Now;
+            historyEntry.CreatedByUserId = userId;
 
+            _ctx.ProductHistories.Add(historyEntry);
+
+            _ctx.Products.Remove(product);
+
+            await _ctx.SaveChangesAsync();
+        }
     }
 
 }
