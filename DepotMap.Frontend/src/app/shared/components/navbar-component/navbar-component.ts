48b90e2598d9a93fb26645a2f109c4995fe564db
Kóbori Bence <kobori.bence@stud.uni-obuda.hu>
2026-03-25 20:14:31 +0100

    basic navbar created

--- DepotMap.Frontend/src/app/shared/components/navbar-component/navbar-component.ts
+++ DepotMap.Frontend/src/app/shared/components/navbar-component/navbar-component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../core/services/auth-service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-navbar-component',
@@ -7,5 +9,25 @@ import { Component } from '@angular/core';
   styleUrl: './navbar-component.scss',
 })
 export class NavbarComponent {
+  isLoggedIn$!: Observable<boolean>;
 
+  navItems: NavItem[] = [
+    new NavItem('Inventory',   '/inventory'),
+    new NavItem('Warehouse',   '/warehouse'),
+    new NavItem('Products',    '/products'),
+    new NavItem('Procurement', '/procurement'),
+    new NavItem('Orders',      '/orders'),
+    new NavItem('Movements',   '/movements'),
+    new NavItem('Users',       '/users'),
+  ];
+}
+
+export class NavItem {
+  label: string;
+  route: string;
+
+  constructor(label: string, route: string) {
+    this.label = label;
+    this.route = route;
+  }
 }
