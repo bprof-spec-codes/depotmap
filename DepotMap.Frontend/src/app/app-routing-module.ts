62e17dc2263f7dfee382fe34255a7a590d37e069
rezsnyakdani <rezsnyakd@gmail.com>
2026-04-02 17:21:05 +0200

    import order-list, order-create, order-edit components to app-routing-module

--- DepotMap.Frontend/src/app/app-routing-module.ts
+++ DepotMap.Frontend/src/app/app-routing-module.ts
@@ -4,11 +4,19 @@ import { LoginComponent } from './features/auth/login-component/login-component'
 import { ProductsListComponent } from './features/products/products-list/products-list.component';
 import { ProductCreateComponent } from './features/products/product-create/product-create.component';
 import { ProductEditComponent } from './features/products/product-edit/product-edit.component';
+import { OrderList } from './features/orders/order-list/order-list';
+import { OrderCreate } from './features/orders/order-create/order-create';
+import { OrderEdit } from './features/orders/order-edit/order-edit';
+
+
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'products/create', component: ProductCreateComponent },
   { path: 'products/edit/:id', component: ProductEditComponent },
   { path: 'products', component: ProductsListComponent },
+  { path: 'orders/create', component: OrderCreate },
+  { path: 'orders/edit/:id', component: OrderEdit },
+  { path: 'orders', component: OrderList }
 ];
 
 @NgModule({
