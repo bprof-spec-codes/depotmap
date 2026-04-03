8d8d6e42ba1978577cc60eb7499dbbd50e932dac
rezsnyakdani <rezsnyakd@gmail.com>
2026-04-03 09:36:45 +0200

    add IProductStockLogic

---
+++ DepotMap.Backend/DepotMap.Logics/Interfaces/IProductStockLogic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DepotMap.Entities.Models.DTOs.StockMovement;
+
+namespace DepotMap.Logics.Interfaces
+{
+    public interface IProductStockLogic
+    {
+        Task<IEnumerable<ProductStockViewDto>> GetAllStocksAsync();
+        Task<IEnumerable<ProductStockViewDto>> GetStocksByProductAsync(string productId);
+        Task<IEnumerable<ProductStockViewDto>> GetStocksByCompartmentAsync(string compartmentId);
+    }
+}
