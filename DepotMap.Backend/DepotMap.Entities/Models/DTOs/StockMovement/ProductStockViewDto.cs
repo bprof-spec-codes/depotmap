231004e0ebba786bb98e72484ed728ee294d41c4
rezsnyakdani <rezsnyakd@gmail.com>
2026-04-03 09:34:53 +0200

    add ProductStockViewDto

---
+++ DepotMap.Backend/DepotMap.Entities/Models/DTOs/StockMovement/ProductStockViewDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DepotMap.Entities.Models.DTOs.StockMovement
+{
+    internal class ProductStockViewDto
+    {
+    }
+}
