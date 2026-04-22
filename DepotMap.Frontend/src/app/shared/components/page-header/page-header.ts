804897857831822e1180e6b2f5e7da912b5139e1
OrcsikRoland <orcsikroland21@gmail.com>
2026-04-22 18:15:20 +0200

    Refactor product edit and list components for improved UI and functionality
    - Updated styles in product edit component for better layout and responsiveness.
    - Introduced new content card component for consistent card design across the application.
    - Enhanced product list component with a new header and improved search functionality.
    - Refactored HTML structure in product list for better readability and maintainability.
    - Added new SCSS files for base styles, buttons, cards, forms, layout, states, tables, and tokens to streamline styling.
    - Implemented responsive design adjustments for various components to enhance user experience on different screen sizes.

---
+++ DepotMap.Frontend/src/app/shared/components/page-header/page-header.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-page-header',
+  standalone: false,
+  templateUrl: './page-header.html',
+  styleUrl: './page-header.scss',
+})
+export class PageHeader {
+
+  @Input() title = '';
+  @Input() subtitle = '';
+  @Input() hasActions = false;
+}
